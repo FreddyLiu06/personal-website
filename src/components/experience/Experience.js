9a3fc7c3286f8d7fdc90a7d5c2de7df1180f094b
FreddyLiu06 <103144599+FreddyLiu06@users.noreply.github.com>
2023-07-25 17:40:16 +0800

    updates

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -9,17 +9,17 @@ const Experience = () => {
             <div className = "experiences-entry">
                 <h3 className="entryTitle"><b>Undergraduate Researcher</b> | University of Pennsylvania</h3>
                 <p className="entryDates">January 2023 - Present</p>
-                <p className="entryText">Assist in <b>wearable free hand gesture recognition</b> using wireless sensing technology and machine learning. Supervised by <a href="https://cis.upenn.edu/~mingminz" target="_blank" style={{textDecoration: 'underline', color: 'blue'}}>Professor Mingmin Zhao</a>. Improve and calibrate vision-based system to incorperate <b>3D ground truth sensor</b> and improve training model performance.</p>
+                <p className="entryText">Achieve <b>panoramic radar imaging</b> by combining signal processing and machine learning techniques. Supervised by <a href="https://cis.upenn.edu/~mingminz" target="_blank" style={{textDecoration: 'underline', color: 'blue'}}>Professor Mingmin Zhao</a>. Improve ground truth data labelling effiency by 90% by <b>iteratively training</b> a <b>depth map segmentation model</b> to automate tasks. Enable <b>circular object detection</b> capabilities by implementing a custom <b>panoramic Regional Proposal Network</b>. Integrated with a multi-task model network built on the <b>Detectron2</b> library.</p>
             </div>
             <div className = "experiences-entry">
                 <h3 className="entryTitle"><b>Teaching Assistant</b> | University of Pennsylvania</h3>
                 <p className="entryDates">August 2022 - Present</p>
-                <p className="entryText">Working as a teaching assistant for the class <b>Data Structures and Algorithms</b>. Hold <b>weekly office hours</b> for a class of <i>200+ students</i> to understand topics and homework questions. <b>Co-lead weekly recitation</b> for group of 15 students to review material. Write homework questions, obfuscations, and solutions. <b>Plan and run review sessions</b> on topics including complexity theory, divide and conquer algorithms, graph algorithms, hashing, binary trees, and more.</p>
+                <p className="entryText">Working as a teaching assistant for the class <b>Data Structures and Algorithms</b>.Assist <b>200+ students</b> with understanding topics and homework questions by holding office hours. <b>Lead weekly recitation</b> for group of 15 students to review material. Write homework questions, obfuscations, and solutions. <b>Plan and run review sessions</b> on topics including <b>complexity theory, divide and conquer algorithms, graph algorithms, hashing, binary trees, and more.</b></p>
             </div>
             <div className="experiences-entry">
                 <h3 className="entryTitle"><b>Software Engineering Intern</b> | Sengital Limited</h3>
                 <p className="entryDates">May 2022 - August 2022</p>
-                <p className="entryText">Undertook <b>outsourcing software development project</b> for investment company client Gravity Capital Partners. Developed impact measurement and management platform to evaluate 60 start-up projects and determine investment targets. Collaborated with team of 3 to implement web application by delegating individual and collaborative tasks, accomplished using <i>React</i> on the front-end and <i>NodeJS express</i> and <i>MariaDB</i> on the server.</p>
+                <p className="entryText">Undertook <b>outsourcing software development project</b> for investment company client Gravity Capital Partners. Developed impact measurement and management platform to evaluate 60 start-up projects and determine investment targets. Facilitated <b>100% of startup assessments</b> by engineering web application from scratch, using <i>ReactJS</i> on frontend, and <i>NodeJS</i> and <i>MariaDB</i> on the server.</p>
             </div>
             <div className="experiences-entry">
                 <h3 className="entryTitle"><b>Vision Programmer</b> | Penn Aerial Robotics</h3>
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -8,9 +8,14 @@ const Projects = () => {
             <div className="projectss page">
             <div className="projects-entry">
                 <h3 className="projectname">Traffic Sign Recognition</h3>
-                <p className="projectdescription">Investigated applying various <b>classical machine learning models</b>, such as K-Nearest Neighbors, Logistic Regression, and Support Vector Machines, as well as <b>deep learning methods</b> such as Convolutional Neural Networks and transfer learning to a <b>traffic sign classification task</b>. Utilized <b>data augmentation</b>, <b>k-fold cross validation</b> hyperparameter tuning, and other techniques to optimize model performances. Methods were compared when trained on both a large dataset, and a small dataset to simulate a scenario when data is scarce. Evaluated model performance using F1 score, LIME, and CNN activation visualizations.</p>
+                <p className="projectdescription">Solved traffic sign classification task by applying various <b>classical machine learning models</b>, such as K-Nearest Neighbors, Logistic Regression, and Support Vector Machines, as well as <b>deep learning methods</b> such as Convolutional Neural Networks and transformers. Optimized model performing by utilizing <b>data augmentation</b>, <b>k-fold cross validation</b> hyperparameter tuning, and other techniques. Methods were compared when trained on both a large dataset, and a small dataset to simulate a scenario when data is scarce. Evaluated model performance using F1 score, LIME, and CNN activation visualizations.</p>
                 <Button variant="contained" className="button" size="large" href="https://github.com/willjhliang/traffic-sign-recognition" target="_blank">View Project</Button>
-                <Button variant="contained" className="button" size="large" href="https://github.com/willjhliang/traffic-sign-recognition/blob/main/paper.pdf" target="_blank">Paper</Button>
+                <Button variant="contained" className="button" size="large" href="https://github.com/willjhliang/traffic-sign-recognition/blob/main/paper.pdf" target="_blank">Report</Button>
+            </div>
+            <div className="projects-entry">
+                <h3 className="projectname">NBA Insider Portal</h3>
+                <p className="projectdescription">Developed web application that users to query and compare NBA statistics for teams and players. Reduced average database query time by <b>70%</b> using <b>query optimization</b> and <b>relational schema design</b> techniques. Enhanced <b>data quality, integrity, and efficiency</b> by leveraging <i>pandas</i> to clean dataset. Engineered interactive user interface using <i>ReactJS</i> on the front-end, and <i>NodeJS</i> on the back-end, linking to <i>MySQL</i> database hosted on <i>AWS EC2</i> instance.</p>
+                <Button variant="contained" className="button" size="large" href="https://github.com/FreddyLiu06/NBA_Portal" target="_blank">View Project</Button>
             </div>
             <div className="projects-entry">
                 <h3 className="projectname">Bunked Roommate Matching</h3>
@@ -20,14 +25,6 @@ const Projects = () => {
             <div className="projects-entry">
                 <h3 className="projectname">Wireless Fall Detection Sensor</h3>
                 <p className="projectdescription">Investigated wearable-free sensing and fall detection in individual research project supervised by Professor Wang at the Hong Kong Polytechnic University. Gathered 8 first-hand datasets of 100+ instances each using a TI mmWave FMCW sensor and processed data to train and create support vector machine model using Python sklearn library. Created prototype device by combing model with signal processing.</p>
-            </div>
-            <div className="projects-entry">
-                <h3 className="projectname">Bookshelf Project</h3>
-                <p className="projectdescription">Created bookshelf web application to track of books read and personal book ratings. Developed using React on the frontend and Django on the backend, using Google books API for book data.</p>
-            </div>
-            <div className="projects-entry">
-                <h3 className="projectname">Snake Game</h3>
-                <p className="projectdescription">Developed snake game in Java from scratch using object oriented programming techniques.</p>
             </div>
             </div>
         </div>
