c17736e2eed8bee8ac0873d249b600effe5240f6
FreddyLiu06 <103144599+FreddyLiu06@users.noreply.github.com>
2023-07-25 18:10:56 +0800

    intro update

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -7,10 +7,9 @@ const Intro = () => {
         <div className="intro" id="intro">
             <div className="left">
                 <div className="wrapper">
-                    <h1>Hello!</h1>
+                    <h1>Hello</h1>
                     <h2>I'm <b>Freddy</b>, a student at the University of Pennsylvania pursuing a bachelor's degree in Computer Science </h2>
-                    <p>Currently, I'm focused on exploring my interests in machine learning, algorithms, and other areas of computer science through my classes and personal projects. Outside of school, I enjoy staying active: playing basketball and going on hikes (take a look at some of my photos below!). I also enjoy reading to learn more about the world.</p>
-                    <p>Thank you for taking the time to look at this website!</p>
+                    <p>Currently, I'm focused on exploring my interests in machine learning, algorithms, and other areas of computer science through my classes and research projects. In my free time I enjoy playing basketball, hiking and crocheting. Thank you for taking a look at my page.</p>
                     <div className="buttons">
                         <Button variant="contained" className="button" startIcon={<LinkedIn />} size="large" href="https://www.linkedin.com/in/freddy-liu-153983206/" target="_blank">LinkedIn</Button>
                         <Button variant="contained" className="button" startIcon={<GitHub />} size = "large" href="https://github.com/FreddyLiu06" target="_blank">Github</Button>
